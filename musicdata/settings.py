1765ab5ca9e7af4a6e35909d5adbfc1f388d1799
Jamaal Bernabe <jnbernabe@gmail.com>
2025-12-09 16:17:04 -0500

    feat: Initialize Django project settings, add Render deployment configuration, and create requirements file.

--- musicdata/settings.py
+++ musicdata/settings.py
@@ -20,10 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wc)f7%*b&4djhi%g$i0sjn9g-a5x3722$mqdyoe^ioehp!y59m'
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-wc)f7%*b&4djhi%g$i0sjn9g-a5x3722$mqdyoe^ioehp!y59m')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'musicinsights.onrender.com,localhost,127.0.0.1').split(',')
 
